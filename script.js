0b68314e3c8fceb038b8d83d519d8cf786a9dab4
RaphaelB00 <109770676+RaphaelB00@users.noreply.github.com>
2022-08-04 19:13:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate random variables //
 function randomInt(min, max) {
   if (!max) {
     max = min
@@ -14,6 +15,7 @@ function getRandomItem(list) {
   return list [randomInt(list.length)]
 }
 
+// First Password Question & Requirements //
 function generatePassword() {
 
   var userInput = window.prompt("How many characters would you like your password to be?")
@@ -29,17 +31,20 @@ function generatePassword() {
     window.alert("Password length must be between 8-128 characters.")
     return
   }
-
+  
+ // Further Questions User Sees //
   var userWantsNumbers = window.confirm("Would you like numbers?")
   var userWantsSymbols = window.confirm("Would you like symbols?")
   var userWantsLowercase = window.confirm("Would you like lowercase letters?")
   var userWantsUppercase = window.confirm("Would you like uppercase letters?")
 
+ // List of Possible Password Outcomes if User selects "Okay" for the given variable // 
   var numberlist = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var symbollist = ["!", "@", "#", "$", "%", "&", "*"]
   var lowercaselist = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var uppercaselist = []
 
+ // Options for password - Numbers, Symbols, Lowercase, Uppercase //
   var optionsCart = []
 
   for (var i = 0; i < lowercaselist.length; i++) {
@@ -62,10 +67,12 @@ function generatePassword() {
     optionsCart.push(uppercaselist)
   }
 
+ // If User does not select any variables - Automatcally creates all lowercase password //
   if (optionsCart.length === 0) {
     optionsCart.push(lowercaseList)
   }
 
+ // Generate random list of variables for  desired password //
   var generatePassword = ""
 
   for (var i = 0; i < passwordLength; i++) {
